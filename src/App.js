4a08dae6e5c5ce0781adfff5d28223c94dec3168
garyCoffey <garycoffey12@gmail.com>
2018-11-27 09:53:52 -0600

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>test</h1>
         <BrowserRouter>
           <div>
             <Route exact path="/" component={HomePage} />
